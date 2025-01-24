637be3aea9e989e8c4fcbd42752ca3415fc33782
Deepeshnama <deepeshnama0786@gmail.com>
2025-01-24 19:40:13 +0530

    added

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ menuIcon.onclick = () => {
 
     function NewTab() {
       window.open(
-        "https://drive.google.com/file/d/17FnIcbBioqcdo7XNsBzcOtzf7MUFxJ22/view?usp=sharing",
+        "https://drive.google.com/file/d/180MexUTNVMDFtYWQBJOwAtiq5tC5i8XR/view?usp=sharing",
         "_blank"
       );
     }
